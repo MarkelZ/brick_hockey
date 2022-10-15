349df52a8dde9daaa445d416052e7ca5222e4e9b
markelz <markel.xubi@gmail.com>
2022-10-15 19:36:56 +0200

    Added shooting and not shooting states.

--- src/ballshooter.py
+++ src/ballshooter.py
@@ -10,42 +10,58 @@ class BallShooter:
         self.x = table.width / 2
         self.y = table.height - 16
         self.ball = Ball(table, Vector2(self.x, self.y), Vector2(1, 1))
+        self.num_balls = 10
+        self.balls_to_shoot = 0
+        self.is_shooting = False
+        self.shoot_delay = 3
+        self.shoot_ctr = 0
+        self.shoot_direction = None
 
     def _get_mouse_direction(self):
         return pygame.mouse.get_pos() - Vector2(self.x, self.y)
 
     def update(self):
-        # Debug
-        if pygame.key.get_pressed()[pygame.K_SPACE]:
-            self.shoot_ball()
+        if self.is_shooting:
+            if self.balls_to_shoot <= 0:
+                self.is_shooting = False
+            else:
+                if self.shoot_ctr <= 0:
+                    self.shoot_ctr = self.shoot_delay
+                    self.table.balls.append(
+                        Ball(self.table, Vector2(self.x, self.y), self.shoot_direction))
+                    self.balls_to_shoot -= 1
+                else:
+                    self.shoot_ctr -= 1
 
-    def shoot_ball(self):
-        self.table.balls.append(
-            Ball(self.table, Vector2(self.x, self.y), self._get_mouse_direction()))
+    def shoot_balls(self):
+        self.is_shooting = True
+        self.balls_to_shoot = self.num_balls
+        self.shoot_direction = self._get_mouse_direction()
 
-    def draw(self, sfc):
+    def draw(self, sfc, draw_laser=False):
         # Draw laser line
-        # Give ball direction towards mouse pointer
-        direction = self._get_mouse_direction().normalize()
-        self.ball.d = Ball.DEFAULT_SPEED * direction
+        if draw_laser:
+            # Give ball direction towards mouse pointer
+            direction = self._get_mouse_direction().normalize()
+            self.ball.d = Ball.DEFAULT_SPEED * direction
 
-        # Calculate numsteps amount of steps of the ball
-        numsteps = 256
-        xx = [self.ball.p.x]
-        yy = [self.ball.p.y]
-        for _ in range(numsteps):
-            self.ball.step(False)
-            xx.append(self.ball.p.x)
-            yy.append(self.ball.p.y)
+            # Calculate numsteps amount of steps of the ball
+            numsteps = 256
+            xx = [self.ball.p.x]
+            yy = [self.ball.p.y]
+            for _ in range(numsteps):
+                self.ball.step(False)
+                xx.append(self.ball.p.x)
+                yy.append(self.ball.p.y)
 
-        # Draw line segments of laser in reverse order to avoid overlap
-        xyb = list(zip(xx, yy, np.linspace(1, 0.5, numsteps)))
-        xyb.reverse()
-        prev = Vector2(xx[-1], yy[-1])
-        for (x, y, bright) in xyb:
-            pos = Vector2(x, y)
-            pygame.draw.line(sfc, (255 * bright, 0, 0), prev, pos, width=4)
-            prev = pos
+            # Draw line segments of laser in reverse order to avoid overlap
+            xyb = list(zip(xx, yy, np.linspace(1, 0.5, numsteps)))
+            xyb.reverse()
+            prev = Vector2(xx[-1], yy[-1])
+            for (x, y, bright) in xyb:
+                pos = Vector2(x, y)
+                pygame.draw.line(sfc, (255 * bright, 0, 0), prev, pos, width=4)
+                prev = pos
 
         # Draw ball
         self.ball.p = Vector2(self.x, self.y)
--- src/gamestate.py
+++ src/gamestate.py
@@ -1,3 +1,4 @@
+from lzma import is_check_supported
 import pygame
 from table import Table
 from ballshooter import BallShooter
@@ -9,15 +10,26 @@ class GameState:
         self.table = Table()
         self.bs = BallShooter(self.table)
         self.dw = DebugWriter()
+        self.is_shooting = False
 
     def update(self):
         self.dw.clear()
         self.bs.update()
         self.table.update()
-        self.dw.writeln('Balls: ' + str(len(self.table.balls)))
-        self.dw.writeln('Bricks: ' + str(len(self.table.bricks)))
+        self.dw.writeln(
+            'Balls: ' + str(self.bs.num_balls if self.is_shooting else self.bs.balls_to_shoot))
+
+        if self.is_shooting:
+            # If space is pressed, shoot balls
+            if pygame.key.get_pressed()[pygame.K_SPACE]:
+                self.bs.shoot_balls()
+                self.is_shooting = False
+        else:
+            # If all balls are gone, switch to shooting state
+            if len(self.table.balls) == 0 and not self.bs.is_shooting:
+                self.is_shooting = True
 
     def draw(self, sfc):
-        self.bs.draw(sfc)
+        self.bs.draw(sfc, draw_laser=self.is_shooting)
         self.table.draw(sfc)
         self.dw.draw(sfc)
