46d584b596904b6548817f7427be6da6f4cb3011
markelz <markel.xubi@gmail.com>
2022-10-15 21:46:36 +0200

    Added minimum shoot angle.

--- src/ballshooter.py
+++ src/ballshooter.py
@@ -14,30 +14,43 @@ class BallShooter:
         self.balls_to_shoot = 0
         self.is_shooting = False
         self.shoot_delay = 3
-        self.shoot_ctr = 0
+        self.shoot_timer = 0
         self.shoot_direction = None
+        self.min_slope = -0.1
 
     def _get_mouse_direction(self):
-        return pygame.mouse.get_pos() - Vector2(self.x, self.y)
+        direction = pygame.mouse.get_pos() - Vector2(self.x, self.y)
+        # If the slope of the direction is too horizontal,
+        # then return a vector with the minimum allowed slope
+        if direction.x != 0 and direction.y / abs(direction.x) >= self.min_slope:
+            return Vector2(np.sign(direction.x), self.min_slope)
+        else:
+            return direction
 
     def update(self):
         if self.is_shooting:
+            # If there are no balls to shoot left, stop shooting
             if self.balls_to_shoot <= 0:
                 self.is_shooting = False
             else:
-                if self.shoot_ctr <= 0:
-                    self.shoot_ctr = self.shoot_delay
-                    self.table.balls.append(
-                        Ball(self.table, Vector2(self.x, self.y), self.shoot_direction))
+                # If timer is at 0, shoot
+                if self.shoot_timer <= 0:
+                    ball = Ball(self.table, Vector2(self.x, self.y),
+                                self.shoot_direction)
+                    self.table.balls.append(ball)
                     self.balls_to_shoot -= 1
+                    self.shoot_timer = self.shoot_delay
+                # If timer is not at 0, decrease timer by 1
                 else:
-                    self.shoot_ctr -= 1
+                    self.shoot_timer -= 1
 
+    # Start shooting balls (self.num_balls many)
     def shoot_balls(self):
         self.is_shooting = True
         self.balls_to_shoot = self.num_balls
         self.shoot_direction = self._get_mouse_direction()
 
+    # Draw ballshooter
     def draw(self, sfc, draw_laser=False):
         # Draw laser line
         if draw_laser:
